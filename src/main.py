cf9f0578f0d4c1b72cc907f31bbe22f44814d168
daniel166 <daniel.zlacky@gmail.com>
2020-07-14 11:09:34 +0200

    handle typo by levenshtein

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import typer
 from typing import List
+from model.Book import Book
 
 
 def main(title: List[str]):
     typer.echo(f"Book title: {title}")
+    books = Book()
+    books.recommend(' '.join(title))
 
 
 if __name__ == "__main__":
