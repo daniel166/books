692507c53590dcbb0ffaf575b2c2280441c36ea1
daniel166 <daniel.zlacky@gmail.com>
2020-07-16 15:13:32 +0200

    recommendation

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
 import typer
 from typing import List
-from model.Book import Book
+from model.Books import Books
 
 
 def main(title: List[str]):
-    typer.echo(f"Book title: {title}")
-    books = Book()
-    books.recommend(' '.join(title))
+    typer.echo(f'Book title: {title}')
+    title = ' '.join(title).lower()
+    books = Books()
+
+    # if title is not present in dataset, compute Levenshtein distance and print 10 most similar titles
+    if title not in books.titles():
+        raise ValueError(books.similar_titles(title))
+
+    # book name can be ambiguous, same title different authors
+    authors = books.authors(title)
+    if len(authors) > 1:
+        author = typer.prompt(f'{title} is ambigious. Please specify author surname. Posibilities are: {authors}:')
+    else:
+        author = list(authors)[0].lower()
+        # TODO check if user typed correct author name
+
+    books.recommend(title, author)
 
 
 if __name__ == "__main__":
